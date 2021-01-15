c54ea423b0485772a76800b7ce466765a522623f
Abdullah Saydemir <abdullah.saydemir@ozu.edu.tr>
2021-01-15 14:30:49 +0300

    Body and form is done. some little tweaks may occur

---
+++ my-app/src/Components/AbdullahPage/AbdullahBody.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import './AbdullahBody.css';
+import logo from '../../images/abdullah.jpg'; // with import
+
+class AbdullahBody extends React.Component{
+    render(){
+        return <div id = "abdüBody"> 
+                    <br/><br/><br/><br/><br/><br/>
+                    <img src={logo} id = "abdü" alt="Abdullah Saydemir" width="200" height="200"></img>
+                    <article>
+
+                        <h1>Abdullah Saydemir</h1>
+
+                        <h2>Some do it with a bitter look,
+                        Some with a flattering word
+                        </h2>
+
+                            <p id = "abdüP">
+
+                            I don't have much to say about myself <b><b>but</b>
+                            </b> there is an appalling story about three lizards.
+                            Originally the story is written in Turkish, so I will continue Turkish.
+                            If I did not know Turkish, I would learn just to understand this. <br/><br/>
+
+
+                            Zamanın birinde 3 tane kertenkele varmış. Bunlar baya iyi kardeşlermiş. Yedikleri içtikleri ayrı
+                            gitmezmiş. Bunların ağzına kadar dolu bir yemek depoları ve lükse kaçmayan ama ihtiyaçlarına da
+                            cevap veren bir evleri varmış. Bunların tek kuralları evden çıkış ve eve geliş sıralarıymış. Evden
+                            ilk küçük çıkar ve en son da eve o gelirmiş.
+                            Günün birinde bir ejderha bunlar evde yokken evlerine gelmiş, yemeklerinden çalabildiklerini çalmış
+                            sonra kalanları yakmış, evi de yıkıp gitmiş. Akşama doğru eve gelen büyük kertenkele evi ve depoyu
+                            görüp çok sinirlenmiş, ejderhanın ayak izlerini takip etmiş. Ejderhaya yaklaşırken ejderha onun
+                            geldiğini
+                            fark etmiş ve bir kuyruk darbesiyle onu öldürmüş. Büyük ejderhanın cesedi savrulup kül olan evin
+                            yakınlarına
+                            düşmüş.<br/><br/>
+
+                            Daha sonra ortanca eve gelmiş, bakmış abisi ölmüş, ev yanıyor depodaki yemekler de çalınmış. Çok
+                            sinirlenmiş. Sinirle o da ejderhanın ayak izlerini takip etmiş. Büyük kertenkeleden daha fazla yaklaşmış
+                            ejderhaya
+                            fakat
+                            o da başaramamış. Ejderha bir alevle onu küle çevirmiş. Ortancanın sadece iskeleti kalmış ortada.<br/><br/>
+                        
+                            Küçük eve gelmiş. Bakmış abisi ölü, ortanca yok, ev yanıyor, depo bomboş. O da sinirlenmiş fakat
+                            olanları
+                            anlayıp sinirle hareket etmemiş. Gece olana kadar bir yer bulmuş ve uzunca ne yapacağını düşünmüş.
+                            Planını
+                            uygulamak için yanına <b><i>sivri bir kurşun kalem, boyunun yarısı kadar bir bez parçası</i></b> ve
+                            <b><i> dayanıklı
+                                    bir urgan</i></b> almış ve yola koyulmuş. Yolda yürürken ortancanın iskeletini görmüş, birkaç
+                            saniye gözyaşı
+                            dökmüş ama intikam alacağını düşünüp bu duyguyu hemen savuşturmuş. Uzaktan ejderhayı görmüş. Ejderha
+                            büyük
+                            bir mağaranın içinde sırtı taşa gelecek şekilde yatıyormuş.<br/><br/>
+                        
+                            Küçük kertenkele iyice yaklaşmış fakat sonra ses çıkarabileceği aklına gelmiş. Bezden biraz koparmış ve
+                            ayaklarını
+                            o bezle güzelce kaplamış bu sayede yürürken artık ses çıkmıyormuş. Sonra mağaranın içine girmiş.
+                            Ejderhanın arkasına
+                            geçmek için duvardan yürümüş. Arkasına geçince eline sivri kurşun kalemi almış. Sonra ejderha onu da
+                            görmüş ve öldürmüş.<br/><br/>
+                            </p>
+                    </article>
+                    <a href="https://github.com/Saydemr" id = "github" class="fa fa-github fa-3x" ></a>
+                    <br/><br/>               
+                </div>
+                
+
+    }
+}
+
+export default AbdullahBody;
---
+++ my-app/src/Components/AbdullahPage/AbdullahForm.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import './AbdullahForm.css';
+
+class AbdullahForm extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            name: "",
+            surname:"",
+            email:"",
+            phoneNumber:"",
+            message:"",
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit = e => {
+        const {name, surname, email, phoneNumber, message} = this.state;
+
+        var student = {
+            "firstname": name,
+            "surname": surname,
+            "email": email,
+            "phoneNumber": phoneNumber,
+            "message": message
+        }
+        localStorage.setItem(student.email,JSON.stringify(student));
+    }
+
+
+   
+    render(){
+        return  <div className = "message-form">
+                    <form id ="action-form">
+
+                            <label for="name" className ="inplabel"><b>Name</b></label><br/>
+                            <input type="text" className="input" onChange={evt => this.setState({ name: evt.target.value})} id="name" maxlength="20" pattern="^[A-Za-z -]+$" required />
+                            <br/><br/>
+
+                            <label for="surname" className="inplabel"><b>Surname</b></label><br/>
+                            <input type="text" className="input" onChange={evt => this.setState({ surname: evt.target.value})} id="surname"  maxlength="20" pattern="^[A-Za-z -]+$" required/> <br/><br/>
+
+                            <label for="email" className="inplabel"><b>Email</b></label><br/>
+                            <input type="email" className="input" onChange={evt => this.setState({ email: evt.target.value})} id="email"  required/> <br/><br/>
+
+                            <label for="phoneNumber" className="inplabel"><b>Phone Number</b></label><br/>
+                            <input type="text" className="input" onChange={evt => this.setState({ phoneNumber: evt.target.value})} id="phoneNumber"  required/> <br/><br/>
+
+                            <label for="message" className="inplabel"><b>Message</b></label><br/>
+                            <textarea type="text" className="input" onChange={evt => this.setState({ message: evt.target.value})} id="message"  maxlength="1000" name="message" required></textarea>
+                            <br/><br/>
+
+                            
+
+                            <button type="submit" className="abdüButton" onClick= {this.handleSubmit} >Submit</button>
+                            <br/><br/>
+                    </form>
+
+
+                </div>
+    }
+}
+
+export default AbdullahForm;
