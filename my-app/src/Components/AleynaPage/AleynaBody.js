16849f90fb0907b758b8c9541f87710a41e6f831
aleynaolmezcan <aleyna.olmezcan@ozu.edu.tr>
2021-01-16 21:59:53 +0300

    Redesign form

--- my-app/src/Components/AleynaPage/AleynaBody.js
+++ my-app/src/Components/AleynaPage/AleynaBody.js
@@ -36,19 +36,19 @@ class Popup extends React.Component {
                     <div class="aleynaPopup_inner">
                             <form action="">
                                 <h3>Contact Form</h3>
-                                <label id = "aleynaLabel" for="name"><b>Name</b></label><b></b>
+                                <label id = "aleynaLabel" for="name"><b>Name</b></label><b></b><br></br>
                                 <input type="text" id = "name" onChange={evt => this.setState({ name: evt.target.value})} placeholder="Enter Your Name" name="name" required/><br/>
                 
-                                <label id = "aleynaLabel" for="surname"><b>Surname</b></label>
+                                <label id = "aleynaLabel" for="surname"><b>Surname</b></label><br></br>
                                 <input type="text" id = "surname" onChange={evt => this.setState({ surname: evt.target.value})} placeholder="Enter Your Surname" name="surname" required/><br/>
                 
-                                <label id = "aleynaLabel" for="email"><b>Email</b></label>
+                                <label id = "aleynaLabel" for="email"><b>Email</b></label><br></br>
                                 <input type="text" id = "email" onChange={evt => this.setState({ email: evt.target.value})}  placeholder="Enter Your Email" name="email" required/><br/>
                 
-                                <label id = "aleynaLabel" for="phone"><b>Phone Number</b></label>
+                                <label id = "aleynaLabel" for="phone"><b>Phone Number</b></label><br></br>
                                 <input type="text" id = "phone" onChange={evt => this.setState({ phoneNumber: evt.target.value})} placeholder="Enter Your Phone Number" name="phone" required/><br/>
                 
-                                <label id = "aleynaLabel" for="message"><b>Message</b></label>
+                                <label id = "aleynaLabel" for="message"><b>Message</b></label><br></br>
                                 <input type="text" id = "message" onChange={evt => this.setState({ message: evt.target.value})}  placeholder="Enter Your Message" name="message" maxlength="1500" required/><br/>
                 
                                 <button type="submit" id="submitButton" onClick= {this.handleSubmit} >Submit</button>
